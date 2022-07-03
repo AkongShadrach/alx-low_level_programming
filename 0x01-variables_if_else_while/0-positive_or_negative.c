2d85aa6218ad66ca2aaf476898fd7c13659b5d8d
AkongShadrach <shadrachakong@gmail.cim>
2022-07-03 13:37:54 -0700

    My target commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,50 @@
+{#include<stdlib.h>
+
+#include<time.h>
+
+#include<stdio.h>
+
+
+
+	/**
+	 *
+	 * *main->assignarandomnumbertothevariableneachtimeitisexecuted
+	 *
+	 * *andprintthelastdigitofthenumberstoredinthevariablen
+	 *
+	 * *Return:Always0(Success)
+	 *
+	 * */
+
+	intmain(void)
+
+	{
+
+		intn;
+
+
+
+		srand(time(0));
+
+		n=rand()-RAND_MAX/2;
+
+		if(n>0)
+
+				printf("%dispositive\n",n);
+
+		if(n==0)
+
+				printf("%diszero\n",n);
+
+		if(n<0)
+
+
+
+				printf("%disnegative\n",n);
+
+	}
+
+	return(0);
+
+}
 

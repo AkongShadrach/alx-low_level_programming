954f0af38cb43d0f16c8bd1e83a547eda7fb5ad7
AkongShadrach <shadrachakong@gmail.cim>
2022-07-06 15:39:14 -0700

    My target commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of the input
+ * @n: the variable to be checked
+ * created: Shdrach_Akong
+ * cc: 6 july, 2022
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}

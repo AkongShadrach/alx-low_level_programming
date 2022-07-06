ef6acd483c22fd81c1ca3b27c7c77385318cf55c
AkongShadrach <shadrachakong@gmail.cim>
2022-07-06 14:21:27 -0700

    My target commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lower case
+ * created : Shadrach_Akong
+ * cc: 6 july, 2022
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int ii;
+
+	for (ii = 'a'; ii <= 'z'; ii++)
+		_putchar(ii);
+	_putchar('\n');
+}

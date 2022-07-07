0f81644b1462dd9fc10d960741496f30a1cd456e
AkongShadrach <shadrachakong@gmail.cim>
2022-07-06 23:29:01 -0700

    My target commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,26 +8,42 @@
 
 void times_table(void)
 {
-	int ii, jj, mul;
+int num, mult, prod;                                                                                                                                   
 
-	for (ii = 0; ii <= 9; ii++)
-	{
-		for (jj = 0; jj <= 9; jj++)
-		{
-			mul = ii * jj;
-			if (jj == 0)
-				_putchar('0' + mul);
-			else if (mul < 10)
-			{
-				_putchar(' ');
-				_putchar('0' + mul);
-			}
-			if (jj < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
-}
+                                                                                                                                                       
+
+for (num = 0; num <= 9; num++)                                                                                                                         
+
+{                                                                                                                                                      
+
+	_putchar('0');                                                                                                                                         
+
+	for (mult = 1; mult <= 9; mult++)                                                                                                                      
+
+	{                                                                                                                                                      
+
+		_putchar(',');                                                                                                                                         
+
+		_putchar(' ');                                                                                                                                         
+
+		prod = num * mult;                                                                                                                                     
+
+		if (prod <= 9)                                                                                                                                         
+
+			_putchar(' ');                                                                                                                                         
+
+		else                                                                                                                                                   
+
+			_putchar((prod / 10) + '0');                                                                                                                           
+
+		_putchar((prod % 10) + '0');                                                                                                                           
+
+	}                                                                                                                                                      
+
+	_putchar('\n');                                                                                                                                        
+
+}                                                                                                                                                      
+
+}           
+
+
